85e61e264104ebd2034582be2441e46cfc131810
chetan <zambare.c123@gmail.com>
2020-11-24 11:01:19 +0530

    [Chetan Added]: Select position to be mark Method Added

--- src/TikTacToeGame.java
+++ src/TikTacToeGame.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class TikTacToeGame {
 	
 	static char [] board = new char[10];
+	static int [] boardFill = {0,0,0,0,0,0,0,0,0,0};
 	static char userMove ;
 	static char computerMove;
 	
@@ -18,7 +19,31 @@ public class TikTacToeGame {
 		System.out.println(" 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 ");
 		System.out.println("Current board position is follows ");
 		System.out.println(showcurrentBoard());
+		
+		int number = userTurn();
+		System.out.println(showcurrentBoard());
 	}
+	
+	/*userTurn method  player asked to choose position to mark entry
+	 to check entry fill condition boaedFill will be check if 0 then return will executed
+	 */
+	
+	private static int userTurn() {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Select the index position where to place mark ");
+			int number = sc.nextInt(); 
+			if (boardFill[number] == 0) {
+				boardFill[number] = 1;
+				board[number] = userMove;
+				return number;
+			}
+		}
+		
+	}
+	
+
+	
 	//show current board
 	private static String showcurrentBoard() {
 		return board[1] + " | " + board[2] + " | " + board[3] + "\n" + board[4] + " | " + board[5] + " | " + board[6] + "\n" + board[7] + " | " + board[8] + " | " + board[9];
