5e553dff51b3a05b6a7cd905e657ae4665b394b7
chetan <zambare.c123@gmail.com>
2020-11-24 10:02:42 +0530

    [Chetan Added]: Create move

--- src/TikTacToeGame.java
+++ src/TikTacToeGame.java
@@ -1,19 +1,37 @@
+import java.util.Scanner;
+
 
 public class TikTacToeGame {
 	
-	static char [] Board = new char[10];
+	static char [] board = new char[10];
+	static char userMove ;
+	static char computerMove;
 	
 	public static void main(String[] args) {
 		System.out.println(" Welcome start game");
 		TikTacToeGame game = new TikTacToeGame();
 		game.createBoard();
+		game.chooseMove();
+		System.out.println( "UserMove : " +userMove + " ComputerMove : " + computerMove);
 	}
 	
 	//method created for createBoard
 	public void createBoard() {
-		for (char obj : Board) {
+		for (char obj : board) {
 			obj = " ".charAt(0);
 		}
 	}
-
+	
+	public void chooseMove() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please choose what you want to choose X or O ");
+		String mark = sc.next();
+		userMove = mark.charAt(0);
+		if (mark.equals("X")) {
+			computerMove = "O".charAt(0);
+		}
+		else {
+			computerMove = "X".charAt(0);
+		}
+	}
 }
